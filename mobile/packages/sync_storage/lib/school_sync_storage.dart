b04f4e4f476c2f1464235f326930c874d6a568d8
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-07-30 05:08:57 +0600

    feat(MOB-01): export scope-bound sync payload protection

--- mobile/packages/sync_storage/lib/school_sync_storage.dart
+++ mobile/packages/sync_storage/lib/school_sync_storage.dart
@@ -2,5 +2,6 @@ library;
 
 export 'src/file_encrypted_sync_store.dart';
 export 'src/sync_key_vault.dart';
+export 'src/sync_payload_protector.dart';
 export 'src/sync_record_cipher.dart';
 export 'src/sync_storage_scope.dart';
