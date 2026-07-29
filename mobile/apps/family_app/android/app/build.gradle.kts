622f5e710d4f7ba22a7102c03b9b651e56003752
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-07-30 01:58:46 +0600

    security(MOB-01): prohibit debug signing for family release

--- mobile/apps/family_app/android/app/build.gradle.kts
+++ mobile/apps/family_app/android/app/build.gradle.kts
@@ -15,10 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.ozzyl.school.family"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -33,9 +30,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Release signing is injected only by the controlled store pipeline.
         }
     }
 }
