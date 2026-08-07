2ffa8a1718df3d41ccb8a1f46759af7dc6305504
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-08-07 08:40:03 +0600

    fix(mobile): remove redundant lifecycle import

--- mobile/packages/app_bootstrap/lib/src/coordinator.dart
+++ mobile/packages/app_bootstrap/lib/src/coordinator.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:school_api_client/mobile_bootstrap_api.dart';
 import 'package:school_api_client/school_api_client.dart';
