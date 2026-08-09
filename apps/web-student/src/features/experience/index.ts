907dedb2eb572a9126346ca6dd2a70938d70acc9
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-08-09 09:39:14 +0600

    feat(student): export focused published records workspace

--- apps/web-student/src/features/experience/index.ts
+++ apps/web-student/src/features/experience/index.ts
@@ -2,3 +2,4 @@ export * from './StudentCommunicationsWorkspace';
 export * from './StudentDailyWorkspace';
 export * from './StudentDocumentsReportingWorkspace';
 export * from './StudentExperienceShell';
+export * from './StudentPublishedRecordsWorkspace';
